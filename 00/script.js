9c7523b8701d5258fdaaa01df43c88e4cb59a72a
DhruvinSathvara07 <dhruvinsathvara05@gmail.com>
2026-02-02 22:35:54 +0530

    Changes #

--- 00/script.js
+++ 00/script.js
@@ -1,3 +1,20 @@
-const scene = new THREE.scene();
+// cdn
+import * as THREE from 'https://unpkg.com/three@0.155.0/build/three.module.js';
 
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4);
+
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+renderer.render(scene, camera);
